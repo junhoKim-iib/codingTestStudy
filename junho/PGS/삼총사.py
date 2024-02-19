5e51a397b4b3d4b43ef4afa53d91a22d4e89a19e
kimjunho <59608767+junhoKim-iib@users.noreply.github.com>
2024-02-19 05:26:18 +0000

    [PGS - L1]삼총사

---
+++ junho/PGS/삼총사.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+
+def solution(number):
+    answer = 0
+    for combi in combinations(number,3):
+        if sum(combi) == 0:
+            answer += 1
+    return answer
