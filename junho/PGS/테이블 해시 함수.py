7cbe33aa41585f5d74a64696a15cf8d9fde522cc
kimjunho <59608767+junhoKim-iib@users.noreply.github.com>
2024-05-10 19:55:09 +0000

    Update [PGS - L2]테이블 해시 함수

--- junho/PGS/테이블 해시 함수.py
+++ junho/PGS/테이블 해시 함수.py
@@ -2,13 +2,11 @@ from functools import reduce
 
 def solution(data, col, row_begin, row_end):
 
-    S_i_list = []
+    answer = 0
     
-    data = sorted(data, key=lambda x : x[0], reverse=True)
-    data = sorted(data, key=lambda x:x[col-1])
+    data = sorted(data, key=lambda x :(x[col-1], -x[0]))
     
-    for i in range(row_begin - 1, row_end):
-        S_i_list.append(sum(val%(i+1) for val in data[i]))
+    answer = reduce(lambda x,y: x ^ y,
+                    [sum(map(lambda x : x%(i+1),data[i])) for i in range(row_begin - 1, row_end)])
     
-    
-    return reduce(lambda x,y : x^y, S_i_list)
+    return answer 
